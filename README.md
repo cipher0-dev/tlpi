44df883863a106e50a1dad689237e2638c95135c
c0 <122c597083bd438b7f6d72af75d0259488996477@cipher0.dev>
2025-12-27 13:33:37 -0700

    Initial commit of reading progress

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# The Linux Programming Interface - Michael Kerrisk
+
+This repo tracks my progress on reading TLPI, including any exercises and toy programs I create as tangents to the reading.
+
+- [x] 01 - History and Standard
+- [x] 02 - Fundamental Concepts
+- [ ] 03 - System Programming Concepts
---
+++ ch2/unsafe_environ_race.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void* thread_fn(void* val) {
+    setenv("FOO", val, 1);
+    printf("Thread sees FOO=%s\n", getenv("FOO"));
+    return NULL;
+}
+
+int main() {
+    setenv("FOO", "hello", 1);
+
+    pthread_t t1, t2;
+
+    pthread_create(&t1, NULL, thread_fn, "test1");
+    pthread_create(&t2, NULL, thread_fn, "test2");
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("\nmain sees FOO=%s\n", getenv("FOO"));
+}
