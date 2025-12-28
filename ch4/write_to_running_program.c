60a505d57ea82cd724550029b684d6665ba9aa04
c0 <122c597083bd438b7f6d72af75d0259488996477@cipher0.dev>
2025-12-28 14:45:37 -0700

    Add program to open a running binary for writing

---
+++ ch4/write_to_running_program.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("usage: %s <file>",argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  int fd = open(argv[1], O_RDWR);
+  if (fd < 0) {
+    perror("open");
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
