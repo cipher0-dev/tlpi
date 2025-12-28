0a041a9dc18827d5b83af51b5fe60e5864c4f3b1
c0 <122c597083bd438b7f6d72af75d0259488996477@cipher0.dev>
2025-12-28 12:19:07 -0700

    Checkpoint on code for ch4

---
+++ ch4/write_ordering.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// NOTE: The writes happen out-of-order since stdio buffers output.
+int main(int argc, char *argv[]) {
+  int size = printf("hello");
+  if (size < 0) {
+    perror("printf");
+    return EXIT_FAILURE;
+  }
+
+  size = write(1, "world\n", 6);
+  if (size < 0) {
+    perror("write");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
