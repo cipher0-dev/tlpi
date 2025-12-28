32c1f9e09caf26f1212092799d7bf2292bde2e9c
c0 <122c597083bd438b7f6d72af75d0259488996477@cipher0.dev>
2025-12-27 19:27:04 -0700

    Add errno and reboot test programs

---
+++ ch3/errno.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+  ssize_t cnt = read(-1, NULL, 10);
+  if (cnt == -1) {
+    perror("read");
+    exit(EXIT_FAILURE);
+  }
+  return EXIT_SUCCESS;
+}
---
+++ ch3/reboot.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <linux/reboot.h>
+#include <sys/syscall.h>
+
+int main(int argc, char *argv[]) {
+  int result = syscall(
+    SYS_reboot,
+    LINUX_REBOOT_MAGIC1,
+    LINUX_REBOOT_MAGIC2C,
+    LINUX_REBOOT_CMD_RESTART,
+    NULL
+  );
+  if (result < 0) {
+    perror("reboot");
+    exit(EXIT_FAILURE);
+  }
+  return EXIT_SUCCESS;
+}
