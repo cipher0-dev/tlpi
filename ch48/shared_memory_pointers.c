10dead152fcf7045a04146fbc4787964c9a5e9a4
c0 <122c597083bd438b7f6d72af75d0259488996477@cipher0.dev>
2026-02-19 08:47:01 -0700

    Fix typo and play with c23 auto

--- ch48/shared_memory_pointers.c
+++ ch48/shared_memory_pointers.c
@@ -6,33 +6,34 @@
 #include <sys/types.h>
 
 void dump_hex32(const void *p) {
-  const unsigned char *b = (const unsigned char *)p;
+  auto b = (const unsigned char *)p;
 
-  for (int i = 0; i < 32; i++) {
-    char *s = (i % 8 == 0 && i != 0) ? " |" : "";
+  for (auto i = 0; i < 32; i++) {
+    auto s = (i % 8 == 0 && i != 0) ? " |" : "";
     printf("%s %02x", s, b[i]);
   }
   printf("\n");
 }
 
 int main(int argc, char *argv[]) {
-  int id = atoi(argv[1]);
-  int value = atoi(argv[2]);
+  auto id = atoi(argv[1]);
+  auto value = atoi(argv[2]);
 
   uint64_t *base = shmat(id, NULL, 0);
   printf("base: %p\n", base);
-  uint64_t *p = base + 1;
+  auto p = base + 1;
   printf("   p: %p\n", p);
 
   dump_hex32(base);
 
   if (*p == 0) {
     printf("p is null, not reading\n");
-    // Store an array offset into the shared memory vs the actual pointer value
-    // since the memory is likely to be located in different address ranges
-    // every time it is mapped.
+    // Store an offset into the shared memory base address vs the actual pointer
+    // value since the memory is likely to be located in different address
+    // ranges every time it is attached. This allows pointers to be position
+    // independent.
     //
-    // Here we use the value 3 to be 24 bytes passed the base.
+    // Here we use the value 3 to be 24 bytes past the base.
     *p = 3;
   } else {
     printf("current value at **p: %ld\n", *(base + *p));
